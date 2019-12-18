a0174389d3a6deb3565be6d3e654696292703db2
Nakarin Satayasai <ParmanBabra@hotmail.com>
2019-12-18 22:39:25 +0700

    update

--- src/main.c
+++ src/main.c
@@ -1,1 +1,122 @@
 
+#include "iot_config.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "iot_system_init.h"
+
+#define LIBRARY_LOG_LEVEL IOT_LOG_INFO
+// Print library name "SAMPLE".
+#define LIBRARY_LOG_NAME "SAMPLE"
+
+#include "iot_logging_setup.h"
+
+static void prvMiscInitialization(void);
+
+int app_main(void)
+{
+    // prvMiscInitialization();
+
+    // if (SYSTEM_Init() == pdPASS)
+    // {
+    //     IotLogInfo("Init pass!");
+    // }
+
+    // vTaskStartScheduler();
+
+    IotLogError("Error.");   // Will be printed because IOT_LOG_ERROR <= LIBRARY_LOG_LEVEL
+    IotLogWarn("Warning. "); // Will be printed because IOT_LOG_WARN <= LIBRARY_LOG_LEVEL
+    IotLogInfo("Info.");     // Will be printed because IOT_LOG_INFO <= LIBRARY_LOG_LEVEL
+    IotLogDebug("Debug.");   // Will not be printed because IOT_LOG_DEBUG > LIBRARY_LOG_LEVEL
+    return 0;
+
+    return 1;
+}
+
+extern void vApplicationIPInit(void);
+static void prvMiscInitialization(void)
+{
+    IotLogInfo("Misc Initialization!");
+}
+
+/*-----------------------------------------------------------*/
+
+extern void esp_vApplicationTickHook();
+void IRAM_ATTR vApplicationTickHook()
+{
+    esp_vApplicationTickHook();
+}
+
+/*-----------------------------------------------------------*/
+extern void esp_vApplicationIdleHook();
+void vApplicationIdleHook()
+{
+    esp_vApplicationIdleHook();
+}
+
+/*-----------------------------------------------------------*/
+
+void vApplicationDaemonTaskStartupHook(void)
+{
+}
+
+/*-----------------------------------------------------------*/
+
+/* configUSE_STATIC_ALLOCATION is set to 1, so the application must provide an
+ * implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
+ * used by the Idle task. */
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
+                                   StackType_t **ppxIdleTaskStackBuffer,
+                                   uint32_t *pulIdleTaskStackSize)
+{
+    /* If the buffers to be provided to the Idle task are declared inside this
+     * function then they must be declared static - otherwise they will be allocated on
+     * the stack and so not exists after this function exits. */
+    static StaticTask_t xIdleTaskTCB;
+    static StackType_t uxIdleTaskStack[configMINIMAL_STACK_SIZE];
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Idle
+     * task's state will be stored. */
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+
+    /* Pass out the array that will be used as the Idle task's stack. */
+    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
+     * Note that, as the array is necessarily of type StackType_t,
+     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+}
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief This is to provide the memory that is used by the RTOS daemon/time task.
+ *
+ * If configUSE_STATIC_ALLOCATION is set to 1, so the application must provide an
+ * implementation of vApplicationGetTimerTaskMemory() to provide the memory that is
+ * used by the RTOS daemon/time task.
+ */
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
+                                    StackType_t **ppxTimerTaskStackBuffer,
+                                    uint32_t *pulTimerTaskStackSize)
+{
+    /* If the buffers to be provided to the Timer task are declared inside this
+     * function then they must be declared static - otherwise they will be allocated on
+     * the stack and so not exists after this function exits. */
+    static StaticTask_t xTimerTaskTCB;
+    static StackType_t uxTimerTaskStack[configTIMER_TASK_STACK_DEPTH];
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Idle
+     * task's state will be stored. */
+    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+
+    /* Pass out the array that will be used as the Timer task's stack. */
+    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
+     * Note that, as the array is necessarily of type StackType_t,
+     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
+}
+/*-----------------------------------------------------------*/
